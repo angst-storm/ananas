46ab9522d7915eb88b671f7c88595e0e9a53abcf
angst-storm <sergey.kiprin@urfu.me>
2022-07-05 19:56:41 +0500

    ананас

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/companies'),
+      dir: require('path').join(__dirname, './coverage/ananas'),
       subdir: '.',
       reporters: [
         { type: 'html' },
